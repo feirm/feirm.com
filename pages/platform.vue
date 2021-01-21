35fd5fd38a7a7796ef850aa181b5cd21dafaa7dc
Jack Coble <jack@coble.uk>
2021-01-21 22:26:45 +0000

    Start of the platform page

---
+++ pages/platform.vue
@@ -0,0 +1,129 @@
+<template>
+  <div>
+    <!-- Landing hero -->
+    <div class="hero is-dark">
+      <div class="hero-body">
+        <div class="container has-text-weight-light">
+          <div class="columns is-centered">
+            <div class="column is-offset-1 is-4">
+              <h2 class="subtitle has-text-weight-light">
+                Introducing the
+                <span class="has-text-warning">Feirm Platform</span>.
+              </h2>
+              <h1 class="title is-1 has-text-weight-light">
+                Secure.
+                <br />
+                Private.
+                <br />
+                Non-custodial.
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- History/background hero -->
+    <div class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns is-left">
+            <div class="column is-4 is-offset-1 has-text-weight-light">
+              <h1 class="title is-2 has-text-weight-light">
+                It is time for a change.
+              </h1>
+
+              <div class="content">
+                <p>
+                  In 2020, there were 5 major hacks which took place on
+                  cryptocurrency platforms/exchanges. The total value of these
+                  hacks summed up to <strong>$286,933,760</strong>.
+                </p>
+
+                <p>
+                  Alongside the initial theft of funds,
+                  <strong>200 pieces</strong> of personally-identifiable
+                  information were breached.
+                </p>
+
+                <p>
+                  <span class="has-text-warning">Feirm</span> is here to solve
+                  this problem by providing a secure, private and non-custodial
+                  cryptocurrency platform for everyone.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- How is the Feirm platform different -->
+    <div class="hero has-background-white-bis">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns is-centered is-multiline">
+            <!-- Differences/features -->
+            <div class="column is-3 has-text-weight-light">
+              <!-- Show some boxes with features -->
+              <div class="box non-custodial">
+                <h2 class="subtitle">Non-custodial</h2>
+                <p>
+                  We do not have access to our users cryptocurrency wallets, nor
+                  do we store any cryptocurrency on behalf of our users. Your
+                  funds are truly yours!
+                </p>
+              </div>
+            </div>
+
+            <div class="column is-3 has-text-weight-light">
+              <div class="box non-custodial">
+                <h2 class="subtitle">State-of-the-art encryption</h2>
+                <p>
+                  End-to-end encryption is used to keep your Feirm account
+                  secure. We do not store your password or any sensitive
+                  information about you. It is encrypted before it even leaves
+                  your device!
+                </p>
+              </div>
+            </div>
+
+            <div class="column is-3 has-text-weight-light">
+              <!-- Show some boxes with features -->
+              <div class="box non-custodial">
+                <h2 class="subtitle">Made with privacy in mind</h2>
+                <p>
+                  Feirm does not want to know anything about our users. We only
+                  require a unique username, password and PIN to create an
+                  account. All of your sensitive information is encrypted and
+                  accessible by you, and only you.
+                </p>
+              </div>
+            </div>
+
+            <div class="column is-3 has-text-weight-light">
+              <div class="box non-custodial">
+                <h2 class="subtitle">Readily available</h2>
+                <p>
+                  Feirm does not rely on mobile application stores for
+                  distribution of the platform application and updates. Instead,
+                  we make use of Progressive Web App (PWA) technology to install
+                  the application directly from your browser.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- List assets -->
+  </div>
+</template>
+
+<style scoped>
+/* Equal box height */
+.box {
+    height: 100%;
+}
+</style>
